82d5cde0d1999558ff7946bbb85aaf89b0296480
WangWindow <1598593280@qq.com>
2025-11-01 15:18:20 +0800

    [upload]: Add some fonts.

---
+++ Fonts/grub-font.sh
@@ -0,0 +1,1 @@
+sudo grub-mkfont -s 48 -o /boot/grub/fonts/font.pf2 ./JetbrainsMono/JetBrainsMonoNerdFont-MediumItalic.ttf
---
+++ Fonts/install.sh
@@ -0,0 +1,4 @@
+sudo cp -r JetbrainsMono/ /usr/share/fonts/
+sudo cp -r sarasa-ui-sc/ /usr/share/fonts/
+
+fc-cache -fv
